fbe33396ea33ea2f84ab3f8f316089e75c93f3b8
akunnen60 <a.kunnen01@gmail.com>
2025-01-03 00:06:50 +0100

    Update cryptoprices.js
    bug fix

--- cryptoprices.js
+++ cryptoprices.js
@@ -1,9 +1,8 @@
 const cryptos = [
-    { symbol: "BTC", feed: "https://api.coingecko.com/api/v3/simple/price?symbol=BTC&vs_currencies=usd" },
+    { symbol: "BTC", feed: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd" },
     { symbol: "ETH", feed: "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd" },
     { symbol: "XRP", feed: "https://api.coingecko.com/api/v3/simple/price?ids=ripple&vs_currencies=usd" },
     { symbol: "SOL", feed: "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd" },
-    { symbol: "BNB", feed: "https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=usd" },
     { symbol: "ADA", feed: "https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=usd" }
 ];
 
